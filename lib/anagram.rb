e399fd716b4879fb54b2ee9b29130d92876c717d
Evans Rutto <robbyevans001@gmail.com>
2022-08-22 19:38:16 +0300

    commit tree 1e8222b935ebadedd11ad56ac391690493933953

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+Your code goes here!
+require"pry"
+class Anagram
+  attr_accessor:name
+  def initialize(name)
+  @name=name
+  end
+  def match(array=[])
+    array.each do|word|
+     if word.chars.sort==@name.chars.sort
+      return [word]
+     else
+      return []
+     end
+    end
+  end
+end
